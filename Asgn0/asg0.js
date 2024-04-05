f541b5f8307020e08016c924f686152753b7f3f6
AQUAiTE <francisiwnlschool@gmail.com>
2024-04-04 22:54:43 -0700

    Part 2 Complete

--- Asgn0/asg0.js
+++ Asgn0/asg0.js
@@ -1,4 +1,31 @@
-// DrawRectangle.js
+// asg0.js
+
+function drawVector(v, color) {
+    /* 
+     * Draws vector "v" as the color "color"
+     */
+    var canvas = document.getElementById('example');
+    if (!canvas) {
+        console.log('Failed to retrieve the <canvas> element');
+        return;
+    }
+
+    var ctx = canvas.getContext('2d');
+
+    // Create path for vector starting from origin
+    ctx.beginPath();
+    var xOrigin = canvas.width / 2;
+    var yOrigin = canvas.height / 2;
+    ctx.moveTo(xOrigin, yOrigin);
+    // Scale coords by 20 to visualize length 1 vectors
+    ctx.lineTo(xOrigin + (v.elements[0] * 20), yOrigin - (v.elements[1] * 20));
+    ctx.closePath();
+
+    // Draw vector as color "color"
+    ctx.strokeStyle = color;
+    ctx.stroke();
+}
+
 function main() {
     // Retrieve <canvas> element <- (1)
     var canvas = document.getElementById('example');
@@ -10,7 +37,11 @@ function main() {
     // Get the rendering context for 2DCG <- (2)
     var ctx = canvas.getContext('2d');
 
-    // Draw a blue rectangle
-    ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set a blue color
-    ctx.fillRect(120, 10, 150, 150); // Fill a rectangle with the color
+    // Instantiate Vector v1 and fill canvas black
+    ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Initialize and draw red vector v1
+    var v1 = new Vector3([2.25, 2.25, 0]);
+    drawVector(v1, "red");
 }
